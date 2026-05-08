cf7f87072461cddb54d83f0714dca63ad87207b6
Matthew Lean <matt@mattlean.com>
2026-05-07 18:05:49 -0700

    Setup ESLint & Prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import js from "@eslint/js"
+import eslintConfigPrettier from "eslint-config-prettier"
+import { defineConfig } from "eslint/config"
+import tseslint from "typescript-eslint"
+
+export default defineConfig(
+	js.configs.recommended,
+	tseslint.configs.recommended,
+	eslintConfigPrettier,
+)
