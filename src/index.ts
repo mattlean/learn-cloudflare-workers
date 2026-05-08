a73003eb53ec4504ca1651c4a03f0c622d378a20
Matthew Lean <matt@mattlean.com>
2026-05-08 10:31:37 -0700

    Create comments API

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,50 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from "hono"
+import { cors } from "hono/cors"
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response("Hello World!")
-	},
-} satisfies ExportedHandler<Env>
+type Bindings = {
+	DB: D1Database
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
+app.use("/api/*", cors())
+
+app.get("/api/posts/:slug/comments", async (c) => {
+	const { slug } = c.req.param()
+	const { results } = await c.env.DB.prepare(
+		"SELECT * FROM comments WHERE post_slug = ?",
+	)
+		.bind(slug)
+		.run()
+	return c.json(results)
+})
+
+app.post("/api/posts/:slug/comments", async (c) => {
+	const { slug } = c.req.param()
+	const { author, body } = await c.req.json<{
+		author: string
+		body: string
+	}>()
+
+	if (!author) return c.text("Missing author value for new comment", 400)
+	if (!body) return c.text("Missing body value for new comment", 400)
+
+	const { success } = await c.env.DB.prepare(
+		"INSERT INTO comments (author, body, post_slug) VALUES (?, ?, ?)",
+	)
+		.bind(author, body, slug)
+		.run()
+
+	if (success) {
+		c.status(201)
+		return c.text("Created")
+	} else {
+		c.status(500)
+		return c.text("Something went wrong")
+	}
+})
+
+app.get("/api/ping", (c) => {
+	return c.text("pong")
+})
+
+export default app
